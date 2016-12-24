ddbb9c0292788f1df66eb316d3cb4b92804795e8
adityaanup <emailadityaanup@gmail.com>
2016-12-24 12:37:23 +0530

    basic routing configured

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ApplicationComponent } from './components/application/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -9,7 +10,7 @@ const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: '', redirectTo: '/home', pathMatch:'full' }
 ];
 
 @NgModule({
